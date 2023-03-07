9aecff151ea52afa3ef7ae78f87c6a7e5b916ae0
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-03-07 13:00:36 -0300

    Git init

---
+++ lib/viavr.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "viavr/version"
+
+module Viavr
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/viavr/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Viavr
+  VERSION = "0.1.0"
+end
